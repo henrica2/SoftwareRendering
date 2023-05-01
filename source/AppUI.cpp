111ed1b2a70f3759549583aee3344b7394b4c5e0
Arnold H <Arnold.Henrickson@igrafx.com>
2023-04-30 17:20:12 -0700

    Init local work

---
+++ source/AppUI.cpp
@@ -0,0 +1,17 @@
+#include "AppUI.h"
+#include "imgui.h"
+
+namespace Lemon
+{
+
+    void RenderUI()
+    {
+        ImGui::Begin("Settings");
+        ImGui::End();
+
+        ImGui::Begin("Viewport");
+        ImGui::End();
+
+        // ImGui::ShowDemoWindow();
+    }
+}
